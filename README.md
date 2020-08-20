e0f3aef7a05eb64fa7f6478e7670596f772be777
Toh <clarenceboonteck.toh@dxc.com>
2020-08-20 13:12:14 +0800

    0.02

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # TestJava
 
 A Java testing playground based on: https://www.tutorialspoint.com/java/index.htm
+
+## Installation
+
+java has to be downloaded and installed. Atom was used as text editor. This project uses jdk-8u261-windows-x64.exe
+
+* [Java SE Development Kit](https://www.oracle.com/java/technologies/java-se-glance.html)
+* [atom 1.43.0](https://atom.io/)
+
+### Atom packages used:
+
+* linter-javac
+
+### General packages:
+
+* atom-beautify
+* busy-signal
+* file-icons
+* intentions
+* linter
+* minimap
+* open_in_cmd
+* project-manager
+* script
---
+++ _01_Classes.java
@@ -0,0 +1,22 @@
+// Demonstrates a basic java program.
+// Class names can only begin with a letter, a currency ($) or _.
+// Java is case sensitive. By convention, class names are written in CamelCase
+// with the first letter in caps. The name of the file should also match the
+// name of the class, including the case.
+// Java also has a list of keywords that cannot be used as names for classes,
+// variables etc. The list can be found here:
+// https://docs.oracle.com/javase/tutorial/java/nutsandbolts/_keywords.html
+
+// Classes can have modifiers, which are classified into access and non access
+// modifiers.
+// Access modifiers: default, public, protected, private.
+// Non access modifiers: final, abstract, strictfp.
+public class _01_Classes {
+
+  // Method names are in camelCase, with the first letter in lower case.
+  // Java program processing starts with the main method, which is neccessary in
+  // every program.
+  public static void main(String []args) {
+    System.out.println("Hello World");
+  }
+}
